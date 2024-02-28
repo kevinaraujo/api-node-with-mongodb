967198df049fbe34eb6018197bc9668ec775eb01
Kevin Araújo <kfa_34@hotmail.com>
2024-02-28 18:04:23 -0300

    feat: Created book route with mocked data.

--- src/app.js
+++ src/app.js
@@ -1,11 +1,26 @@
 import express from 'express';
 
+const books = [
+    {
+        id: 1,
+        title: 'Book 1'
+    },
+    {
+        id: 2,
+        title: 'Book 21'
+    }
+];
+
 const app = express();
 
 app.get('/', (req, res) => {
     res.status(200).send({
         success: 'ok'
     });
+});
+
+app.get('/books', (req, res) => {
+    res.status(200).json(books);
 });
 
 export default app;
