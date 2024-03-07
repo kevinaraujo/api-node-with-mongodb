aaf61309eb21fdfe96703b909e221be76c3e9eeb
Kevin Araújo <kfa_34@hotmail.com>
2024-03-07 20:13:30 -0300

    feat: Added routes to get book by id and update put by id.

--- src/app.js
+++ src/app.js
@@ -11,7 +11,12 @@ const books = [
     }
 ];
 
+const findBooks = (id) => {
+    return books.findIndex(book => book.id === Number(id));
+}
+
 const app = express();
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.status(200).send({
@@ -21,6 +26,29 @@ app.get('/', (req, res) => {
 
 app.get('/books', (req, res) => {
     res.status(200).json(books);
+});
+
+app.post('/books', (req, res) => {
+    books.push(req.body);
+
+    res.status(201).json({
+        "message": "success"
+    })
+});
+
+app.get('/books/:id', (req, res) => {
+    console.log('id =>',req.params.id);
+    const index = findBooks(req.params.id);
+
+    res.status(200).json(books[index]);
+});
+
+app.put('/books/:id', (req, res) => {
+    const index = findBooks(req.params.id);
+    
+    books[index].title = req.body.title;
+
+    res.status(200).json(books)
 });
 
 export default app;
